ddd3667e55921b14ad3e98ecdf4fde9f12ca88c4
devendervb12 <info@s-max.in>
2017-11-21 20:43:30 +0530

    initial commit

--- WebContent/Component.js
+++ WebContent/Component.js
@@ -1,9 +1,31 @@
 //create a sub class of sap.ui.core.UIComponent
 sap.ui.core.UIComponent.extend("smax.ui.empReg.Component", {
 	
-	createContent : function(){		
-		return new sap.m.Button({ text : "Go"});		
+	metadata : {
+		rootView : "smax.ui.empReg.view.App",
+		routing : {
+			 config : {
+				 routerClass : "sap.m.routing.Router"
+			 },
+			 routes : [
+				 {
+					 pattern : "abc",
+					 name : "firstView",
+					 viewName : "smax.ui.empReg.view.InitialPage",
+					 viewType : "XML",
+					 controlAggregation : "pages",
+					 controlId : "idApp",
+					
+					 
+				 }
+			 ]
+		 }
+	},
+	init : function(){
+		sap.ui.core.UIComponent.prototype.init.apply(this);
+		this.getRouter().initialize();
 	}
+	
 });
 
 
